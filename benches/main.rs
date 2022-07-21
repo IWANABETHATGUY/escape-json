7906cfb49a8a6fb1591c250e2a79eaa778842630
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-07-21 17:33:21 +0800

    refactor: 💡 refactor macro name

--- benches/main.rs
+++ benches/main.rs
@@ -2,19 +2,19 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use escape_json::{escape_json, two_pass_replace, two_pass_search_one_pass_copy, regex_replace, regex_iter_replace};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    test!("array.json", c);
-    test!("big.json", c);
-    test!("escape.json", c);
-    test!("large.json", c);
-    test!("package.json", c);
-    test!("escape_heavy.json", c);
+    bench!("array.json", c);
+    bench!("big.json", c);
+    bench!("escape.json", c);
+    bench!("large.json", c);
+    bench!("package.json", c);
+    bench!("escape_heavy.json", c);
 }
 
 criterion_group!(benches, criterion_benchmark);
 criterion_main!(benches);
 
 #[macro_export]
-macro_rules! test {
+macro_rules! bench {
     ($filename:expr, $c:ident) => {{
         let source = include_str!(concat!("../assets/", $filename));
         $c.bench_function(concat!($filename, " @`two_pass_replace`"), |b| {
